9fd73b13173aafb7904124fc4e4055a630bc2f40
Akshay Dagade <akdagade@gmail.com>
2018-03-13 03:34:09 +0530

    Easy

--- 27. Remove Element/main.py
+++ 27. Remove Element/main.py
@@ -1,31 +1,37 @@
-nums = [2]
-n = 2
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        l = len(nums)
+        f = 0
+        n=val
+        if l==1 and nums[0]==n:
+            del nums[0]
+            return 0
+        
+        if l==0:
+            return 0
 
+        while f<l-1:
+            front = nums[f]
+            last =  nums[l-1]
 
-l = len(nums)
-f = 0
+            if front==n and last!=n:
+                nums[f] = last
+                f = f+1
+                l = l-1
 
-if l==1 and nums[0]==n:
-	del nums[0]
-	exit()
+            elif (last==n):
+                l = l-1
 
+            elif front!=n and last!=n:
+                f = f+1	
 
-while f<l-1:
-	front = nums[f]
-	last =  nums[l-1]
-	
-	if front==n and last!=n:
-		nums[f] = last
-		f = f+1
-		l = l-1
-	
-	elif (last==n):
-		l = l-1
-
-	elif front!=n and last!=n:
-		f = f+1	
-
-if(nums[l-1]==n):
-	l=l-1
-
-print (str(l) + '===' + str(nums[:l])) 
+        if(nums[l-1]==n):
+            l=l-1
+            
+        
+        return l
